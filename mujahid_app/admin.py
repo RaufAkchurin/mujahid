b6bf84c3c2db5f8471ec288468c6ac173d774b95
rauf.akchurin <abdurauf.dev@gmail.com>
2023-12-07 23:28:31 +0500

    new

--- mujahid_app/admin.py
+++ mujahid_app/admin.py
@@ -3,4 +3,9 @@ from django.contrib.admin import site
 
 from mujahid_app.models import Day
 
-site.register(Day)
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ("date", "quran", "salat", "soum", "apathy_w", "time_pc", "time_wash", "looked_down", "dont_watch", "tg", "weekend", "yout_t", "az_subh", "az_asr", "masjeed", "happy_l")
+
+
+site.register(Day, DayAdmin)
--- mujahid_app/models.py
+++ mujahid_app/models.py
@@ -6,31 +6,46 @@ class Day(models.Model):
     awoke = models.TimeField(null=True)
     sunnah_duration = models.IntegerField(blank=True, null=True)
     sleep = models.TimeField(blank=True, null=True)
+    weekend = models.BooleanField(default=False)
 
                     #education
     education_time = models.IntegerField(blank=True, null=True)
     din_ed_time = models.IntegerField(blank=True, null=True)
 
                     #deen
-    azkar_subh = models.BooleanField(blank=True)
-    azkar_asr = models.BooleanField(blank=True)
-    quran_str = models.IntegerField(blank=True, null=True)
+    az_subh = models.BooleanField(blank=True)
+    az_asr = models.BooleanField(blank=True)
+    quran = models.IntegerField(blank=True, null=True)
     masjeed = models.IntegerField(blank=True, null=True)
+    soum = models.BooleanField(default=False)
+    salat = models.BooleanField(default=False)
 
                     #work
-    work_time_pc = models.IntegerField(blank=True, null=True)
-    work_time_wash = models.IntegerField(blank=True, null=True)
+    apathy_w = models.BooleanField(default=False)  # apathy_to_work
+    time_pc = models.IntegerField(blank=True, null=True)  # work_time_pc
+    time_wash = models.IntegerField(blank=True, null=True)  # work_time_wash
+    money_pc = models.IntegerField(blank=True, null=True)
+    money_wm = models.IntegerField(blank=True, null=True)
+
 
                     #addictions
-    problem = models.BooleanField()
-    youtube_time = models.IntegerField(blank=True, null=True)
+    looked_down = models.BooleanField(verbose_name="Пот.взор")
+    dont_watch = models.BooleanField(verbose_name="Не см.")
+    tg = models.BooleanField()
+    yout_t = models.IntegerField(blank=True, null=True) #youtube
+    tauba = models.BooleanField(default=False)
+    dua = models.BooleanField(default=False)
 
                     #psihosomatics
-    happy_level = models.IntegerField(choices=[(i, str(i)) for i in range(0, 101, 10)])
+    happy_l = models.IntegerField(choices=[(i, str(i)) for i in range(0, 101, 10)])
     difficult_work = models.IntegerField(choices=[(i, str(i)) for i in range(0, 101, 10)])
+    anxiety = models.BooleanField()
     date = models.DateField()
+
+                    #sport
+    sport = models.BooleanField(default=False)
 
 
 
     def __str__(self):
-        return f"work {self.work_time_pc}  n/  |  quran {self.quran_str}  |  ed {self.education_time}  |  {self.date}"
+        return f"work {self.time_pc}  n/  |  quran {self.quran}  |  ed {self.education_time}  |  {self.date} |  {self.happy_l}"
